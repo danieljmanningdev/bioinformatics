85b0731a7dde3d1356e35f7c054989c38feafb3a
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-08-31 00:08:50 +0100

    chore: add comments

--- Go/molecules/transcription.go
+++ Go/molecules/transcription.go
@@ -1,6 +1,29 @@
+//
+// Daniel J. Manning
+//
+// Copyright © 2026 Daniel J. Manning.
+// SPDX-License-Identifier: MIT
+//
+
 package molecules
 
 import "strings"
+
+/*
+   Transcribe a DNA sequence into RNA by replacing thymine with uracil.
+
+   For each nucleotide x:
+
+       f(x) = U, if x = T
+            = x, otherwise
+
+   Alphabets:
+       Σ_DNA = {A, C, G, T}
+       Σ_RNA = {A, C, G, U}
+
+   Transformation:
+       T → U
+*/
 
 func Transcribe(t string) string {
 	return strings.ReplaceAll(t, "T", "U")
